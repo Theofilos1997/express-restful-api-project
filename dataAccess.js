d6510f434f4f831dbde451a695157b0a7579b2b0
Sachinidis <SACHIT@pfizer.com>
2024-08-14 09:24:50 +0300

    Comments deleted from dataAccess.com

--- dataAccess.js
+++ dataAccess.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 
-// Fetch data from the members.json file
 const loadMembers = () => {
     const jsonData = fs.readFileSync(__dirname + "/" + "members.json");
         return JSON.parse(jsonData);
 }
 
-// Update members.js file
 const saveMembers = (newData) => {
     const stringifyData = JSON.stringify(newData);
     fs.writeFileSync(__dirname + "/" + "members.json", stringifyData)
